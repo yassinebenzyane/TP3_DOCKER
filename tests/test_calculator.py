ad39ad30194342e3375cc4744b37cd584aa0f8e1
Yassine <y.benzyane@edu.umi.ac.ma>
2026-03-16 10:09:25 +0000

    Modify test file

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app.calculator import addition, multiplication
+
 
 def test_addition():
     assert addition(2,3) == 5
